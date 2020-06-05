b82736273d0775fe9180bcd7aa5056d150ad2299
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-05 18:47:13 +0900

    Task with parallel running

--- TaskProgramming/Program.cs
+++ TaskProgramming/Program.cs
@@ -11,23 +11,41 @@ namespace TaskProgramming
     {
         static void Main(string[] args)
         {
-            Task<string> task = Task.Run(() =>
-            {
-              return HeavyMethod();
+            // 1の整数を返すHeavyMethod1のTaskを生成します。
+            Task<int> task1 = Task.Run(() => {
+                return HeavyMethod1();
             });
 
-            string result = task.Result;
-            Console.WriteLine(result);            
+            // 2の整数を返すHeavyMethod2のTaskを生成します。
+            Task<int> task2 = Task.Run(() => {
+                return HeavyMethod2();
+            });
+
+            // WhenAllの引数に、先程生成したTaskを入れると、HeavyMethod1、HeavyMethod2が終了するまで
+            // 次のコードに進みません。つまり待機します。
+            Task.WhenAll(task1, task2);
+
+            Console.WriteLine(task1.Result + task2.Result);
+
             Console.ReadLine();
         }
 
-        static string HeavyMethod()
+        static int HeavyMethod1()
         {
             Console.WriteLine("すごく重い処理その1(´・ω・｀)はじまり");
-            Thread.Sleep(3000);
+            Thread.Sleep(2000);
             Console.WriteLine("すごく重い処理その1(´・ω・｀)おわり");
 
-            return "Hoge";
+            return 1;
+        }
+
+        static int HeavyMethod2()
+        {
+            Console.WriteLine("すごく重い処理その2(´・ω・｀)はじまり");
+            Thread.Sleep(2000);
+            Console.WriteLine("すごく重い処理その2(´・ω・｀)おわり");
+
+            return 2;
         }
     }
 }
