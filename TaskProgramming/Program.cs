6fffb04a511a314fa5094633e65712eb946dc316
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-05 17:04:19 +0900

    Legacy Async Thread

---
+++ TaskProgramming/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                HeavyMethod1();
+            }));
+
+            thread.Start();
+            HeavyMethod2();
+            Console.ReadLine();
+        }
+
+        static void HeavyMethod1()
+        {
+            Console.WriteLine("すごく重い処理その1(´・ω・｀)はじまり");
+            Thread.Sleep(5000);
+            Console.WriteLine("すごく重い処理その1(´・ω・｀)おわり");
+        }
+
+        static void HeavyMethod2()
+        {
+            Console.WriteLine("すごく重い処理その2(´・ω・｀)はじまり");
+            Thread.Sleep(3000);
+            Console.WriteLine("すごく重い処理その2(´・ω・｀)おわり");
+        }
+    }
+}
