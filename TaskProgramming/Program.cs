cddcecc3a0df60ac4c3c8a15ce533a5ef9dbf3f5
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-05 22:03:39 +0900

    parameterize thread

--- TaskProgramming/Program.cs
+++ TaskProgramming/Program.cs
@@ -11,41 +11,30 @@ namespace TaskProgramming
     {
         static void Main(string[] args)
         {
-            // 1の整数を返すHeavyMethod1のTaskを生成します。
-            Task<int> task1 = Task.Run(() => {
-                return HeavyMethod1();
-            });
+            Console.WriteLine("Please enter the target number");
+            object target = Console.ReadLine();
+            // Create an instance of ParameterizedThreadStart delegate
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(Number.PrintNumbers);
+            Thread T1 = new Thread(parameterizedThreadStart);
+            // Pass the traget number to the start function, which
+            // will then be passed automatically to PrintNumbers() function
+            //Thread thread = new Thread(Number.PrintNumbers); //★このようにも書けます。CompilerはConvertしてくれますから
+            T1.Start(target);
+        }     
+    }
 
-            // 2の整数を返すHeavyMethod2のTaskを生成します。
-            Task<int> task2 = Task.Run(() => {
-                return HeavyMethod2();
-            });
-
-            // WhenAllの引数に、先程生成したTaskを入れると、HeavyMethod1、HeavyMethod2が終了するまで
-            // 次のコードに進みません。つまり待機します。
-            Task.WhenAll(task1, task2);
-
-            Console.WriteLine(task1.Result + task2.Result);
-
-            Console.ReadLine();
-        }
-
-        static int HeavyMethod1()
+    class Number
+    {
+        public static void PrintNumbers(object target)
         {
-            Console.WriteLine("すごく重い処理その1(´・ω・｀)はじまり");
-            Thread.Sleep(2000);
-            Console.WriteLine("すごく重い処理その1(´・ω・｀)おわり");
-
-            return 1;
-        }
-
-        static int HeavyMethod2()
-        {
-            Console.WriteLine("すごく重い処理その2(´・ω・｀)はじまり");
-            Thread.Sleep(2000);
-            Console.WriteLine("すごく重い処理その2(´・ω・｀)おわり");
-
-            return 2;
+            int number = 0;
+            if (int.TryParse(target.ToString(), out number))
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
