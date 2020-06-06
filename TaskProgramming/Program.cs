852f2894e5a65a547abb6862753b109f97d16de2
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-06 12:11:25 +0900

    learning task...

--- TaskProgramming/Program.cs
+++ TaskProgramming/Program.cs
@@ -15,34 +15,58 @@ namespace TaskProgramming
             Console.WriteLine("Please enter the target number");
             // Read from the console and store it in target variable
             int target = Convert.ToInt32(Console.ReadLine());
-            // Create an instance of the Number class, passing it
-            // the target number that was read from the console
-            Number number = new Number(target);
-            // Specify the Thread function
-            Thread T1 = new Thread(new ThreadStart(number.PrintNumbers));
-            // Alternatively we can just use Thread class constructor as shown below
-            // Thread T1 = new Thread(number.PrintNumbers);
+            // Create an instance of callback delegate and to it's constructor
+            // pass the name of the callback function (PrintSumOfNumbers)
+            SumOfNumbersCallback callbackMethod = new SumOfNumbersCallback(PrintSumOfNumbers);
+            // Create an instance of Number class and to it's constrcutor pass the target
+            // number and the instance of callback delegate
+            Number number = new Number(target, callbackMethod);
+            // Create an instance of Thread class and specify the Thread function to invoke
+            Thread T1 = new Thread(new ThreadStart(number.ComputeSumOfNumbers));
             T1.Start();
 
             Console.ReadLine();
-        }     
+        }
+
+        // Callback method that will receive the sum of numbers
+        public static void PrintSumOfNumbers(int sum)
+        {
+            Console.WriteLine("Sum of numbers is " + sum);
+        }
     }
+
+    // Step 1: Create a callback delegate. The actual callback method
+    // signature should match with the signature of this delegate.
+    public delegate void SumOfNumbersCallback(int sumOfNumbers);
 
     class Number
     {
+        // The traget number this class needs to compute the sum of numbers
         int _target;
-        // When an instance is created, the target number needs to be specified
-        public Number(int target)
+
+        // Delegate to call when the the Thread function completes                 
+        // computing the sum of numbers
+        SumOfNumbersCallback _callbackMethod;
+
+        // Constructor to initialize the target number and the callback delegateinitialize
+        public Number(int target, SumOfNumbersCallback callbackMethod)
         {
-            // The targer number is then stored in the class private variable _target
             this._target = target;
+            this._callbackMethod = callbackMethod;
         }
-        // Function prints the numbers from 1 to the traget number that the user provided
-        public void PrintNumbers()
+
+        // This thread function computes the sum of numbers and then invokes
+        // the callback method passing it the sum of numbers
+        public void ComputeSumOfNumbers()
         {
+            int sum = 0;
             for (int i = 1; i <= _target; i++)
             {
-                Console.WriteLine(i);
+                sum = sum + i;
+            }
+            if (_callbackMethod != null)
+            {
+                _callbackMethod(sum);
             }
         }
     }
---
+++ TaskProgramming/ThreadCallBack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using static TaskProgramming.ThreadCallBackMain;
+
+namespace TaskProgramming
+{
+    class ThreadCallBackMain
+    {
+        public delegate void PrintCallback(int result);
+
+        public static void main()
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            PrintCallback objcall = new PrintCallback(PrintResult);
+            Compute objNum = new Compute(input, objcall);
+            Thread thread = new Thread(new ThreadStart(objNum.ComputeNumber));
+            thread.Start();
+        }
+
+        public static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+
+    class Compute
+    {
+        int InputNum;
+
+        PrintCallback _callback;
+
+        public Compute(int num, PrintCallback obj)
+        {
+            InputNum = num;
+            _callback = obj;
+        }
+
+
+        public void ComputeNumber()
+        {
+            InputNum += 10;
+            if (_callback != null)
+            {
+                _callback(InputNum);
+            }
+        }
+    }
+}
